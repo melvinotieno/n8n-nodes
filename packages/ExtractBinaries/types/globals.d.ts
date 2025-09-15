dc3c39b4aabe8b6a73f0953528559a15e7e3b852
Melvin Otieno <o.melvinotieno@gmail.com>
2025-09-15 23:26:41 +0300

    refactor: update global types in globals.d.ts for better integration with n8n-workflow

--- packages/ExtractBinaries/types/globals.d.ts
+++ packages/ExtractBinaries/types/globals.d.ts
@@ -1,8 +1,24 @@
 declare global {
-  interface IOptions {
-    mimeTypes?: string;
-    maxSize?: number;
-  }
+	type IAuthenticateGeneric = import("n8n-workflow").IAuthenticateGeneric;
+
+	type ICredentialTestRequest = import("n8n-workflow").ICredentialTestRequest;
+
+	type ICredentialType = import("n8n-workflow").ICredentialType;
+
+	type IExecuteFunctions = import("n8n-workflow").IExecuteFunctions;
+
+	type INodeExecutionData = import("n8n-workflow").INodeExecutionData;
+
+	type INodeProperties = import("n8n-workflow").INodeProperties;
+
+	type INodeType = import("n8n-workflow").INodeType;
+
+	type INodeTypeDescription = import("n8n-workflow").INodeTypeDescription;
+
+	interface IBinariesOptions {
+		mimeTypes?: string;
+		maxSize?: number;
+	}
 }
 
 export {};
